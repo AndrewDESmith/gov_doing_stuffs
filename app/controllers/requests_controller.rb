f64c867559367377b0197d702596a744d1b6b84a
zhengweiyi <zhengweiyi@gmail.com>
2014-06-21 22:39:20 -0400

    basic flow

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -5,4 +5,23 @@ class RequestsController < ApplicationController
    p @project
 
   end
+
+  def create
+    @project = Project.find(params[:project_id])
+    @request = @project.requests.create(request_params)
+    p @request
+    redirect_to request_path(@request)
+
+  end
+
+  def show
+    @request = Request.find(params[:id])
+    @projects_latest = Project.last(5)
+    p @projects_latest
+  end
+
+  private
+  def request_params
+      params.require(:request).permit(:citizen_name,:email,:twitter_handler,:facebook_handler,:phone,:comment)
+  end
 end
